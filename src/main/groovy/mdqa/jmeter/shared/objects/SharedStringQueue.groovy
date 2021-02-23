39e679d8f9b6753f736d3d923dcdd2bc4d638d2a
mderevyankoqa <mderevyanko@hotmail.com>
2021-02-23 10:21:01 +0200

    Updated Read me, changed the jar name.

--- src/main/groovy/mdqa/jmeter/shared/objects/SharedStringQueue.groovy
+++ src/main/groovy/mdqa/jmeter/shared/objects/SharedStringQueue.groovy
@@ -3,19 +3,20 @@ package mdqa.jmeter.shared.objects
 import java.util.concurrent.ConcurrentLinkedQueue
 
 /**
- * The class represents container of {@link Queue<String>}. Allows you to save and share String objects between different threads and thread groups in the JMeter.
+ * The class represents container of {@link Queue<T>}. Allows you to save and share String objects between different threads and thread groups in the JMeter.
  * @author Michael Derevyanko
  */
-class SharedQueue
+class SharedStringQueue
 {
     private static final Queue<String> sharedQueue = new ConcurrentLinkedQueue<String>()
 
     /**
-     * Gets the instance of the {@link Queue<String>}
-     * @return  the instance of the {@link Queue<String>}
+     * Gets the instance of the {@link Queue<T>}
+     * @return  the instance of the {@link Queue<T>}
      */
     static Queue<String> getInstance()
     {
+
          return sharedQueue
     }
 }
